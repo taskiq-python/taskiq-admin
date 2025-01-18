a18bd54d35d89802e3fb89f3731536507b6c1c30
arturka <yourcamelcase@gmail.com>
2025-01-18 23:29:30 +0100

    fix: corrupted backup download

--- src/server/api/tasks/backup.get.ts
+++ src/server/api/tasks/backup.get.ts
@@ -1,10 +1,13 @@
 import fs from "fs"
+import { db } from "~/server/db"
 import { envVariables } from "~/server/env"
 
 export default defineEventHandler(async (event) => {
-  const stream = fs.createReadStream(envVariables.dbFilePath)
+  await db.$client.backup(envVariables.backupFilePath)
+  const stream = fs.createReadStream(envVariables.backupFilePath)
+
   setHeader(event, "Content-Type", "application/octet-stream")
-  setHeader(event, "Content-Disposition", 'attachment; filename="database.db"')
+  setHeader(event, "Content-Disposition", 'attachment; filename="backup.db"')
 
   return sendStream(event, stream)
 })
