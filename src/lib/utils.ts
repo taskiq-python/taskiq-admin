18302609108056e09d87e2e6aeaa756aac0620e8
arturka <yourcamelcase@gmail.com>
2025-04-21 17:27:39 +0200

    fix: dayjs utc plugin import

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,13 @@
-import dayjs from "dayjs"
-import utc from "dayjs/plugin/utc"
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-import type { TaskSelect } from "~/server/db/schema"
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc.js'
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+import type { TaskSelect } from '~/server/db/schema'
 
 dayjs.extend(utc)
 
 export function formatDate(date: string) {
-  return dayjs.utc(date).local().format("MMM D, YYYY hh:mm A")
+  return dayjs.utc(date).local().format('MMM D, YYYY hh:mm A')
 }
 
 export function cn(...inputs: ClassValue[]) {
@@ -16,7 +16,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export function limitText(text: string, length: number) {
   if (text.length > length) {
-    return text.slice(0, length).trim() + "..."
+    return text.slice(0, length).trim() + '...'
   }
   return text
 }
@@ -25,14 +25,14 @@ export function formatReturnValue(task: TaskSelect) {
   if (task.returnValue?.return_value) {
     return task.returnValue.return_value
   } else {
-    return "null"
+    return 'null'
   }
 }
 
 // temporarily, while dishka hasn't fixed it's module naming bug
 export function formatTaskName(taskName: string) {
-  if (taskName.includes(":")) {
-    const parts = taskName.split(":")
+  if (taskName.includes(':')) {
+    const parts = taskName.split(':')
     return parts[parts.length - 1]
   } else {
     return taskName
